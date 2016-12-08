62cbc418fc79f6902c9e6c0dafb20772d9e9990e
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-12-08 11:03:19 +0100

    Adds logging

--- handlers/api.js
+++ handlers/api.js
@@ -3,13 +3,18 @@
 const Boom = require('boom')
 
 module.exports.getUsersTasks = (request, reply) => {
-  var pattern = {role: 'tasks', type: 'user', user: request.params.userid}
-  var payload = {}
+  const user = request.params.userid
+  const pattern = {role: 'tasks', type: 'user', user: user}
+  const payload = {}
+
+  console.log(`task-front: request tasks for ${user}`)
 
   request.seneca.act(pattern, payload, (error, data) => {
     if (error) {
+      console.log(`task-front: error request tasks for ${user} - ${JSON.stringify(error)}`)
       return reply(Boom.internal(error))
     }
+    console.log(`task-front: returns tasks for ${user}`)
     return reply(data)
   })
 }
