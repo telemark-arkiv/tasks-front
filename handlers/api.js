15b8284f3719a3b6ef25e4d80795d539f58fd162
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-12-09 14:23:29 +0100

    More logging. Returns empty data on error

--- handlers/api.js
+++ handlers/api.js
@@ -1,20 +1,18 @@
 'use strict'
 
-const Boom = require('boom')
-
 module.exports.getUsersTasks = (request, reply) => {
   const user = request.params.userid
   const pattern = {role: 'tasks', type: 'user', user: user}
   const payload = {}
 
-  console.log(`task-front: request tasks for ${user}`)
+  console.log(`tasks-front: request tasks for ${user}`)
 
   request.seneca.act(pattern, payload, (error, data) => {
     if (error) {
-      console.log(`task-front: error request tasks for ${user} - ${JSON.stringify(error)}`)
-      return reply(Boom.internal(error))
+      console.log(`tasks-front: error request tasks for ${user} - ${JSON.stringify(error)}`)
+      return reply({user: user, data: []})
     }
-    console.log(`task-front: returns tasks for ${user}`)
+    console.log(`tasks-front: returns tasks for ${user} - found ${data.data.length}`)
     return reply(data)
   })
 }
